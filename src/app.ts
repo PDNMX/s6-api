ad482799af9cbf0dfe126f71564ea3c7e8346d0c
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2025-06-10 01:05:14 -0600

    add log of requests

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, resp, next) => {
+  const { body } = req;
+  console.log('body: ', body);
+  next();
+});
+
 app.use(`/${config.API_S6_PREFIX}/v1`, routes);
 
 app.listen(config.PORT, () => {
